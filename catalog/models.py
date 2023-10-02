4835a729b9c4c3110a347ec0e85090979f92b321
GeorgijP <georgij.puzanov@inbox.ru>
2023-10-02 16:43:40 +0400

    add migrate

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name_product = models.CharField(max_length=150, verbose_name='наименование')
+    description_product = models.TextField(max_length=1000, verbose_name='описание')
+    image_product = models.ImageField(upload_to= 'image_product/', verbose_name='изображение')
+    category_product = models.CharField(max_length=150, verbose_name='категория')
+    purchase_price_product = models.CharField(max_length=150, verbose_name='цена покупки')
+    date_creation_product = models.CharField(max_length=150, verbose_name='дата создания')
+    date_last_modification_product = models.CharField(max_length=150, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name_product}, {self.description_product}'
+
+    class Meta:
+        verbose_name = 'наименование'
+        verbose_name_plural = 'наименования'
+        ordering = ('name_product', )
+
+class Category(models.Model):
+    name_category = models.CharField(max_length=150, verbose_name='наименование')
+    description_category = models.TextField(max_length=1000, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name_category}, {self.description_category}'
+
+    class Meta:
+        verbose_name = 'наименование'
+        verbose_name_plural = 'наименования'
+        ordering = ('name_category', )
--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homework_19.3',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
