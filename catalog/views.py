3482113a48e7120af43f16d0e126e8a005222ca3
GeorgijP <georgij.puzanov@inbox.ru>
2023-10-17 22:19:30 +0400

    Completed task 20.2 (1,2,3)

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+from catalog.models import Category
+
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    category_list = Category.objects.all()
+
+    context = {
+        'object_list': category_list
+    }
+    return render(request, 'catalog/home.html', context)
+
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
