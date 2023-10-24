77f52d79e4fb55d2330138a8c2492578fe66265a
GeorgijP <georgij.puzanov@inbox.ru>
2023-10-24 12:33:45 +0400

    add new_tags

--- catalog/templatetags/new_tags.py
+++ catalog/templatetags/new_tags.py
@@ -1,3 +1,15 @@
 from django import template
 
 register = template.Library()
+
+
+@register.filter()
+def my_image(value):
+    if value:
+        return f'/image_product/{value}'
+    return '#'
+
+
+@register.simple_tag
+def my_image(value):
+    return f'/image_product/{value}'
